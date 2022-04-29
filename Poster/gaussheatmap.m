557b636b7e4617d0615c86128067eed81de4d4ea
peterha <petercyjha@gmail.com>
2022-04-29 14:25:49 -0400

    Monte carlo sim of trap distribution

--- Poster/gaussheatmap.m
+++ Poster/gaussheatmap.m
@@ -4,20 +4,38 @@
 clear
 close all
 
-n = 5;
+N = 1000; % number of iterations
+n = 5; % number of hotspots/traps
+% no it isn't confusing actually 
 
 layer = 256;
 layers = zeros(layer);
 
 canvas = 1024;
-canvass = 0.5*ones(canvas);
+canvass = 0.5*ones(canvas, canvas, N);
 
-for i = 1:n
-    canvass = canvass+0.5*gauss2d(canvass, 500*rand(1), 1024*rand([1,2]));
-    canvass = canvass-0.5*gauss2d(canvass, 500*rand(1), 1024*rand([1,2]));
+spots = 1024*rand([2*n,2]); % [hotspot x, hotspot y; coldspot x coldspot y; ...]
+hotspots = round(spots(1:2:end,:));
+
+randspots = round(1024*rand([n,2])); % random spots for trad trapping
+
+hauls = zeros(N, 2); % [trad, data; ...]
+
+for j = 1:N
+    for i = 1:n
+        canvass(:,:,j) = canvass(:,:,j)+0.5*gauss2d(canvass(:,:,j), 500*rand(1), spots(2*i-1,:));
+        canvass(:,:,j) = canvass(:,:,j)-0.5*gauss2d(canvass(:,:,j), 500*rand(1), spots(2*i,:));
+        hauls(j,1) = hauls(j,1) + canvass(randspots(i,1),randspots(i,2),j);
+    end
+    for i = 1:n
+        hauls(j,2) = hauls(j,2) + canvass(hotspots(i,1),hotspots(i,2),j);
+    end
 end
+
+nethauls = [sum(hauls(:,1)), sum(hauls(:,2))];
+ratio = nethauls(2)/nethauls(1)
     
-heatmap(canvass)
+heatmap(canvass(:,:,j))
 grid off
 
 function mat = gauss2d(mat, sigma, center)
